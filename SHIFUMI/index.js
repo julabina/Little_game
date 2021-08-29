0d328937b408628f001eccd6fa663b7ee1934e9a
Lenfumé Julien <julabina@hotmail.fr>
2021-08-29 09:19:20 +0200

    29 08 21 09 19

--- SHIFUMI/index.js
+++ SHIFUMI/index.js
@@ -12,7 +12,21 @@ const resultContainer = document.querySelector(".resultContainer");
 const resultContainerLS = document.querySelector(".resultContainerLS");
 const radioRandom = document.getElementById("radioRandom");
 const radioAI = document.getElementById("radioNormal");
-let choseCpu, result, status, choseAI, prevChoseOdd, prevChoseEven, prevChose;
+const threePts = document.getElementById("radioThreePts");
+const tenPts = document.getElementById("radioTenPts");
+const XPts = document.getElementById("radioXPts");
+const training = document.getElementById("radioTraining");
+let choseCpu,
+  result,
+  status,
+  choseAI,
+  choseAILS,
+  prevChoseOdd,
+  prevChoseEven,
+  prevChose,
+  prevChoseLS,
+  prevChoseEvenLS,
+  prevChoseOddLS;
 let scoreP1 = 0;
 let scoreP2 = 0;
 let scoreP1LS = 0;
@@ -20,9 +34,34 @@ let scoreP2LS = 0;
 let choseP1 = undefined;
 let choseP1LS = undefined;
 let roundAI = 1;
+let roundAILS = 1;
 
 const random = (int) => {
   return Math.floor(Math.random() * int);
+};
+
+const reset = () => {
+  scoreP1 = 0;
+  scoreP2 = 0;
+  choseP1 = undefined;
+  choseCpu = undefined;
+  choseAI = undefined;
+  roundAI = 1;
+  prevChose = undefined;
+  prevChoseEven = undefined;
+  prevChoseOdd = undefined;
+};
+
+const resetLS = () => {
+  scoreP1LS = 0;
+  scoreP2LS = 0;
+  choseP1LS = undefined;
+  choseCpu = undefined;
+  choseAILS = undefined;
+  roundAILS = 1;
+  prevChoseLS = undefined;
+  prevChoseEvenLS = undefined;
+  prevChoseOddLS = undefined;
 };
 
 const AIRandom = () => {
@@ -190,11 +229,86 @@ const statusResultRPS = () => {
 
 function displayRPS() {
   statusResultRPS();
-  resultContainer.innerHTML = `
-  <p>Score : ${scoreP1} - ${scoreP2} : Score</p>
-  <p>P1 : ${choseP1} - ${choseCpu} : CPU</p>
-  <p>Player 1 ${status}</p>  
-  `;
+
+  if (threePts.checked) {
+    if (scoreP1 === 3 || scoreP2 === 3) {
+      if (scoreP1 === 3) {
+        resultContainerLS.innerHTML = `
+          <h2>PLAYER 1 WIN</h2>
+          <p>Score : ${scoreP1} - ${scoreP2} : Score</p>
+          <p>P1 : ${choseP1} - ${choseCpu} : CPU</p>
+        `;
+        reset();
+      } else {
+        resultContainerLS.innerHTML = `
+          <h2>PLAYER 2 WIN</h2>
+          <p>Score : ${scoreP1} - ${scoreP2} : Score</p>
+          <p>P1 : ${choseP1} - ${choseCpu} : CPU</p>
+        `;
+        reset();
+      }
+    } else {
+      resultContainerLS.innerHTML = `
+        <p>Score : ${scoreP1} - ${scoreP2} : Score</p>
+        <p>P1 : ${choseP1} - ${choseCpu} : CPU</p>
+        <p>Player 1 ${status}</p>  
+    `;
+    }
+  } else if (tenPts.checked) {
+    if (scoreP1 === 10 || scoreP2 === 10) {
+      if (scoreP1 === 10) {
+        resultContainerLS.innerHTML = `
+          <h2>PLAYER 1 WIN</h2>
+          <p>Score : ${scoreP1} - ${scoreP2} : Score</p>
+          <p>P1 : ${choseP1} - ${choseCpu} : CPU</p>
+        `;
+        reset();
+      } else {
+        resultContainerLS.innerHTML = `
+          <h2>PLAYER 2 WIN</h2>
+          <p>Score : ${scoreP1} - ${scoreP2} : Score</p>
+          <p>P1 : ${choseP1} - ${choseCpu} : CPU</p>
+        `;
+        reset();
+      }
+    } else {
+      resultContainerLS.innerHTML = `
+        <p>Score : ${scoreP1} - ${scoreP2} : Score</p>
+        <p>P1 : ${choseP1} - ${choseCpu} : CPU</p>
+        <p>Player 1 ${status}</p>  
+    `;
+    }
+  } else if (XPts.checked) {
+    if (scoreP1 === 25 || scoreP2 === 25) {
+      if (scoreP1 === 25) {
+        resultContainerLS.innerHTML = `
+          <h2>PLAYER 1 WIN</h2>
+          <p>Score : ${scoreP1} - ${scoreP2} : Score</p>
+          <p>P1 : ${choseP1} - ${choseCpu} : CPU</p>
+        `;
+        reset();
+      } else {
+        resultContainerLS.innerHTML = `
+          <h2>PLAYER 2 WIN</h2>
+          <p>Score : ${scoreP1} - ${scoreP2} : Score</p>
+          <p>P1 : ${choseP1} - ${choseCpu} : CPU</p>
+        `;
+        reset();
+      }
+    } else {
+      resultContainerLS.innerHTML = `
+        <p>Score : ${scoreP1} - ${scoreP2} : Score</p>
+        <p>P1 : ${choseP1} - ${choseCpu} : CPU</p>
+        <p>Player 1 ${status}</p>  
+    `;
+    }
+  } else {
+    resultContainerLS.innerHTML = `
+    <p>P1 : ${choseP1} - ${choseCpu} : CPU</p>
+    <p>Player 1 ${status}</p>  
+    `;
+    reset();
+  }
 }
 
 /* ----- Rock Papper Scissors Lizard Spock ----- */
@@ -215,8 +329,227 @@ const AIRandomLizardSpock = () => {
   return choseCpu;
 };
 
+const AILizardSpock = () => {
+  if (prevChoseEvenLS == prevChoseOddLS) {
+    if (prevChoseOddLS == "rock") {
+      a = random(4);
+      if (a === 1) {
+        choseAiLS = "papper";
+      } else if (a === 2) {
+        choseAiLS = "scissors";
+      } else if (a === 3) {
+        choseAiLS = "lizard";
+      } else {
+        choseAiLS = "spock";
+      }
+    } else if (prevChoseOddLS == "papper") {
+      a = random(4);
+      if (a === 1) {
+        choseAiLS = "rock";
+      } else if (a === 2) {
+        choseAiLS = "scissors";
+      } else if (a === 3) {
+        choseAiLS = "lizard";
+      } else {
+        choseAiLS = "spock";
+      }
+    } else if (prevChoseOddLS == "scissors") {
+      a = random(4);
+      if (a === 1) {
+        choseAiLS = "rock";
+      } else if (a === 2) {
+        choseAiLS = "papper";
+      } else if (a === 3) {
+        choseAiLS = "lizard";
+      } else {
+        choseAiLS = "spock";
+      }
+    } else if (prevChoseOddLS == "lizard") {
+      a = random(4);
+      if (a === 1) {
+        choseAiLS = "rock";
+      } else if (a === 2) {
+        choseAiLS = "papper";
+      } else if (a === 3) {
+        choseAiLS = "scissors";
+      } else {
+        choseAiLS = "spock";
+      }
+    } else {
+      a = random(4);
+      if (a === 1) {
+        choseAiLS = "rock";
+      } else if (a === 2) {
+        choseAiLS = "papper";
+      } else if (a === 3) {
+        choseAiLS = "scissors";
+      } else {
+        choseAiLS = "lizard";
+      }
+    }
+  } else if (result === 0) {
+    a = random(100);
+    if (choseP1LS == "rock") {
+      if (choseCpu == "papper") {
+        if (a < 45) {
+          choseAiLS = "scissors";
+        } else if (a >= 45 && a < 70) {
+          choseAiLS = "spock";
+        } else if (a >= 70 && a < 80) {
+          choseAiLS = "rock";
+        } else if (a >= 80 && a < 90) {
+          choseAiLS = "lizard";
+        } else {
+          choseAiLS = "papper";
+        }
+      } else {
+        if (a < 45) {
+          choseAiLS = "lizard";
+        } else if (a >= 45 && a < 70) {
+          choseAiLS = "papper";
+        } else if (a >= 70 && a < 80) {
+          choseAiLS = "rock";
+        } else if (a >= 80 && a < 90) {
+          choseAiLS = "scissors";
+        } else {
+          choseAiLS = "spock";
+        }
+      }
+    } else if (choseP1LS == "papper") {
+      if (choseCpu == "scissors") {
+        if (a < 45) {
+          choseAiLS = "rock";
+        } else if (a >= 45 && a < 70) {
+          choseAiLS = "spock";
+        } else if (a >= 70 && a < 80) {
+          choseAiLS = "papper";
+        } else if (a >= 80 && a < 90) {
+          choseAiLS = "lizard";
+        } else {
+          choseAiLS = "scissors";
+        }
+      } else {
+        if (a < 45) {
+          choseAiLS = "rock";
+        } else if (a >= 45 && a < 70) {
+          choseAiLS = "scissors";
+        } else if (a >= 70 && a < 80) {
+          choseAiLS = "papper";
+        } else if (a >= 80 && a < 90) {
+          choseAiLS = "spock";
+        } else {
+          choseAiLS = "lizard";
+        }
+      }
+    } else if (choseP1LS == "scissors") {
+      if (choseCpu == "rock") {
+        if (a < 45) {
+          choseAiLS = "papper";
+        } else if (a >= 45 && a < 70) {
+          choseAiLS = "spock";
+        } else if (a >= 70 && a < 80) {
+          choseAiLS = "lizard";
+        } else if (a >= 80 && a < 90) {
+          choseAiLS = "scissors";
+        } else {
+          choseAiLS = "rock";
+        }
+      } else {
+        if (a < 45) {
+          choseAiLS = "lizard";
+        } else if (a >= 45 && a < 70) {
+          choseAiLS = "papper";
+        } else if (a >= 70 && a < 80) {
+          choseAiLS = "rock";
+        } else if (a >= 80 && a < 90) {
+          choseAiLS = "spock";
+        } else {
+          choseAiLS = "scissors";
+        }
+      }
+    } else if (choseP1LS == "lizard") {
+      if (choseCpu == "rock") {
+        if (a < 45) {
+          choseAiLS = "papper";
+        } else if (a >= 45 && a < 70) {
+          choseAiLS = "spock";
+        } else if (a >= 70 && a < 80) {
+          choseAiLS = "scissors";
+        } else if (a >= 80 && a < 90) {
+          choseAiLS = "lizard";
+        } else {
+          choseAiLS = "rock";
+        }
+      } else {
+        if (a < 45) {
+          choseAiLS = "spock";
+        } else if (a >= 45 && a < 70) {
+          choseAiLS = "rock";
+        } else if (a >= 70 && a < 80) {
+          choseAiLS = "papper";
+        } else if (a >= 80 && a < 90) {
+          choseAiLS = "lizard";
+        } else {
+          choseAiLS = "scissors";
+        }
+      }
+    } else {
+      if (choseCpu == "papper") {
+        if (a < 45) {
+          choseAiLS = "scissors";
+        } else if (a >= 45 && a < 70) {
+          choseAiLS = "lizard";
+        } else if (a >= 70 && a < 80) {
+          choseAiLS = "rock";
+        } else if (a >= 80 && a < 90) {
+          choseAiLS = "spock";
+        } else {
+          choseAiLS = "papper";
+        }
+      } else {
+        if (a < 45) {
+          choseAiLS = "rock";
+        } else if (a >= 45 && a < 70) {
+          choseAiLS = "scissors";
+        } else if (a >= 70 && a < 80) {
+          choseAiLS = "papper";
+        } else if (a >= 80 && a < 90) {
+          choseAiLS = "lizard";
+        } else {
+          choseAiLS = "scissors";
+        }
+      }
+    }
+  } else {
+    a = random(100);
+    if (a < 25) {
+      choseAiLS = "lizard";
+    } else if (a >= 25 && a < 50) {
+      choseAiLS = "spock";
+    } else if (a >= 50 && a < 70) {
+      choseAiLS = "rock";
+    } else if (a >= 70 && a < 90) {
+      choseAiLS = "scissors";
+    } else {
+      choseAiLS = "papper";
+    }
+  }
+  if (roundAILS % 2 == 0) {
+    prevChoseOddLS = choseP1LS;
+  } else {
+    prevChoseEvenLS = choseP1LS;
+  }
+  roundAILS++;
+  prevChoseLS = choseP1LS;
+  choseCpu = choseAiLS;
+};
+
 const resultLizardSpock = () => {
-  AIRandomLizardSpock();
+  if (radioAI.checked) {
+    AILizardSpock();
+  } else {
+    AIRandomLizardSpock();
+  }
   if (choseP1LS == "rock") {
     if (choseCpu == "rock") {
       result = 1;
@@ -228,7 +561,6 @@ const resultLizardSpock = () => {
       result = 2;
     } else {
       result = 0;
-      console.log("test");
     }
   } else if (choseP1LS == "papper") {
     if (choseCpu == "rock") {
@@ -279,31 +611,115 @@ const resultLizardSpock = () => {
       result = 1;
     }
   }
-  console.log(choseCpu);
   return result;
 };
 
 const statusResultLizardSpock = () => {
   resultLizardSpock();
 
-  if (result === 2) {
-    status = "win";
-    scoreP1LS++;
-  } else if (result === 0) {
-    status = "lose";
-    scoreP2LS++;
+  if (training.checked) {
+    if (result === 2) {
+      status = "win";
+    } else if (result === 0) {
+      status = "lose";
+    } else {
+      status = "equal";
+    }
   } else {
-    status = "equal";
+    if (result === 2) {
+      status = "win";
+      scoreP1LS++;
+    } else if (result === 0) {
+      status = "lose";
+      scoreP2LS++;
+    } else {
+      status = "equal";
+    }
   }
 };
 
 function displayLizardSpock() {
   statusResultLizardSpock();
-  resultContainerLS.innerHTML = `
-  <p>Score : ${scoreP1LS} - ${scoreP2LS} : Score</p>
-  <p>P1 : ${choseP1LS} - ${choseCpu} : CPU</p>
-  <p>Player 1 ${status}</p>  
-  `;
+
+  if (threePts.checked) {
+    if (scoreP1LS === 3 || scoreP2LS === 3) {
+      if (scoreP1LS === 3) {
+        resultContainerLS.innerHTML = `
+          <h2>PLAYER 1 WIN</h2>
+          <p>Score : ${scoreP1LS} - ${scoreP2LS} : Score</p>
+          <p>P1 : ${choseP1LS} - ${choseCpu} : CPU</p>
+        `;
+        resetLS();
+      } else {
+        resultContainerLS.innerHTML = `
+          <h2>PLAYER 2 WIN</h2>
+          <p>Score : ${scoreP1LS} - ${scoreP2LS} : Score</p>
+          <p>P1 : ${choseP1LS} - ${choseCpu} : CPU</p>
+        `;
+        resetLS();
+      }
+    } else {
+      resultContainerLS.innerHTML = `
+        <p>Score : ${scoreP1LS} - ${scoreP2LS} : Score</p>
+        <p>P1 : ${choseP1LS} - ${choseCpu} : CPU</p>
+        <p>Player 1 ${status}</p>  
+    `;
+    }
+  } else if (tenPts.checked) {
+    if (scoreP1LS === 10 || scoreP2LS === 10) {
+      if (scoreP1LS === 10) {
+        resultContainerLS.innerHTML = `
+          <h2>PLAYER 1 WIN</h2>
+          <p>Score : ${scoreP1LS} - ${scoreP2LS} : Score</p>
+          <p>P1 : ${choseP1LS} - ${choseCpu} : CPU</p>
+        `;
+        resetLS();
+      } else {
+        resultContainerLS.innerHTML = `
+          <h2>PLAYER 2 WIN</h2>
+          <p>Score : ${scoreP1LS} - ${scoreP2LS} : Score</p>
+          <p>P1 : ${choseP1LS} - ${choseCpu} : CPU</p>
+        `;
+        resetLS();
+      }
+    } else {
+      resultContainerLS.innerHTML = `
+        <p>Score : ${scoreP1LS} - ${scoreP2LS} : Score</p>
+        <p>P1 : ${choseP1LS} - ${choseCpu} : CPU</p>
+        <p>Player 1 ${status}</p>  
+    `;
+    }
+  } else if (XPts.checked) {
+    if (scoreP1LS === 25 || scoreP2LS === 25) {
+      if (scoreP1LS === 25) {
+        resultContainerLS.innerHTML = `
+          <h2>PLAYER 1 WIN</h2>
+          <p>Score : ${scoreP1LS} - ${scoreP2LS} : Score</p>
+          <p>P1 : ${choseP1LS} - ${choseCpu} : CPU</p>
+        `;
+        resetLS();
+      } else {
+        resultContainerLS.innerHTML = `
+          <h2>PLAYER 2 WIN</h2>
+          <p>Score : ${scoreP1LS} - ${scoreP2LS} : Score</p>
+          <p>P1 : ${choseP1LS} - ${choseCpu} : CPU</p>
+        `;
+        resetLS();
+      }
+    } else {
+      resultContainerLS.innerHTML = `
+        <p>Score : ${scoreP1LS} - ${scoreP2LS} : Score</p>
+        <p>P1 : ${choseP1LS} - ${choseCpu} : CPU</p>
+        <p>Player 1 ${status}</p>  
+    `;
+    }
+  } else {
+    resultContainerLS.innerHTML = `
+    <p>P1 : ${choseP1LS} - ${choseCpu} : CPU</p>
+    <p>Player 1 ${status}</p>  
+    `;
+    resetLS();
+  }
 }
 
 /* ----------------- event -----------------------*/
