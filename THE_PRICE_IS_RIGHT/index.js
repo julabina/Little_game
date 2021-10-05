8c9462ad1148c509484aa277bb4b69689fc7325d
Lenfumé Julien <julabina@hotmail.fr>
2021-10-05 21:04:25 +0200

    05 10 21 21 04

--- THE_PRICE_IS_RIGHT/index.js
+++ THE_PRICE_IS_RIGHT/index.js
@@ -11,11 +11,40 @@ const recovery = document.querySelector(".recovery");
 console.log(radios);
 
 let goodPrice, currentPricePropose, countdownVal, gameMod, newTime;
+let vsMode = false;
 
 const randomPrice = (int) => {
   val = Math.random() * int;
   goodPrice = Math.round(val);
   console.log(goodPrice);
+};
+
+const P2Price = (int) => {
+  if (int === 1) {
+    lastPropose.textContent = "Enter your price";
+    document.querySelector(".lastPropose").style.fontSize = "medium";
+    resultStatus.textContent = "between 0 and 100";
+    document.querySelector(".resultStatus").style.fontSize = "medium";
+    vsMode = true;
+  } else if (int === 2) {
+    lastPropose.textContent = "Enter your price";
+    document.querySelector(".lastPropose").style.fontSize = "medium";
+    resultStatus.textContent = "between 0 and 1000";
+    document.querySelector(".resultStatus").style.fontSize = "medium";
+    vsMode = true;
+  } else if (int === 3) {
+    lastPropose.textContent = "Enter your price";
+    document.querySelector(".lastPropose").style.fontSize = "medium";
+    resultStatus.textContent = "between 0 and 10000";
+    document.querySelector(".resultStatus").style.fontSize = "medium";
+    vsMode = true;
+  } else {
+    lastPropose.textContent = "Enter your price";
+    document.querySelector(".lastPropose").style.fontSize = "medium";
+    resultStatus.textContent = "between 0 and 100000";
+    document.querySelector(".resultStatus").style.fontSize = "medium";
+    vsMode = true;
+  }
 };
 
 const test = (time) => {
@@ -74,6 +103,9 @@ const reset = () => {
   more.classList.add("invisible");
   less.classList.add("invisible");
   lastPropose.classList.remove("green");
+  vsMode = false;
+  document.querySelector(".lastPropose").style.fontSize = "40px";
+  document.querySelector(".resultStatus").style.fontSize = "28.8px";
 };
 
 const countdown = (int) => {
@@ -115,15 +147,71 @@ function verify(val) {
   }
 }
 
+const resetStatusText = () => {
+  resultStatus.textContent = "";
+  lastPropose.textContent = "";
+  document.querySelector(".lastPropose").style.fontSize = "40px";
+  document.querySelector(".resultStatus").style.fontSize = "28.8px";
+};
+
 submit.addEventListener("click", () => {
-  if (gameMod === 1) {
-    let valInput = parseInt(playerPropose.value);
-    verify(valInput);
-    playerPropose.value = "";
+  if (radios[9].checked && vsMode == true) {
+    resetStatusText();
+    if (radios[10].checked) {
+      if (playerPropose.value > 100) {
+        resultStatus.textContent = "Between 0 and 100";
+        document.querySelector(".resultStatus").style.fontSize = "medium";
+      } else {
+        goodPrice = parseInt(playerPropose.value);
+        vsMode = false;
+        playerPropose.value = "";
+        resetStatusText();
+        console.log(goodPrice);
+      }
+    } else if (radios[11].checked) {
+      if (playerPropose.value > 1000) {
+        resultStatus.textContent = "Between 0 and 1000";
+        document.querySelector(".resultStatus").style.fontSize = "medium";
+      } else {
+        goodPrice = parseInt(playerPropose.value);
+        vsMode = false;
+        playerPropose.value = "";
+        resetStatusText();
+        console.log(goodPrice);
+      }
+    } else if (radios[12].checked) {
+      if (playerPropose.value > 10000) {
+        resultStatus.textContent = "Between 0 and 10000";
+        document.querySelector(".resultStatus").style.fontSize = "medium";
+      } else {
+        goodPrice = parseInt(playerPropose.value);
+        vsMode = false;
+        playerPropose.value = "";
+        resetStatusText();
+        console.log(goodPrice);
+      }
+    } else {
+      if (playerPropose.value > 100000) {
+        resultStatus.textContent = "Between 0 and 100000";
+        document.querySelector(".resultStatus").style.fontSize = "medium";
+      } else {
+        goodPrice = parseInt(playerPropose.value);
+        vsMode = false;
+        playerPropose.value = "";
+        resetStatusText();
+        console.log(goodPrice);
+      }
+    }
   } else {
-    let valInput = parseInt(playerPropose.value);
-    verify(valInput);
-    playerPropose.value = "";
+    if (gameMod === 1) {
+      let valInput = parseInt(playerPropose.value);
+      verify(valInput);
+      playerPropose.value = "";
+    } else {
+      let valInput = parseInt(playerPropose.value);
+      verify(valInput);
+      playerPropose.value = "";
+    }
   }
 });
 
@@ -157,14 +245,25 @@ startBtn.addEventListener("click", () => {
   }
 
   /* difficulty */
-
-  if (radios[10].checked) {
-    randomPrice(100);
-  } else if (radios[11].checked) {
-    randomPrice(1000);
-  } else if (radios[12].checked) {
-    randomPrice(10000);
-  } else if (radios[13].checked) {
-    randomPrice(100000);
+  if (radios[9].checked) {
+    if (radios[10].checked) {
+      P2Price(1);
+    } else if (radios[11].checked) {
+      P2Price(2);
+    } else if (radios[12].checked) {
+      P2Price(3);
+    } else if (radios[13].checked) {
+      P2Price(4);
+    }
+  } else {
+    if (radios[10].checked) {
+      randomPrice(100);
+    } else if (radios[11].checked) {
+      randomPrice(1000);
+    } else if (radios[12].checked) {
+      randomPrice(10000);
+    } else if (radios[13].checked) {
+      randomPrice(100000);
+    }
   }
 });
