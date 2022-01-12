0f68bc36c73f1968845aa4c10e77a483bb6017fb
Lenfumé Julien <julabina@hotmail.fr>
2022-01-12 14:33:30 +0100

    make html, card css, app.js 60%

---
+++ games/MEMORY/scripts/app.js
@@ -0,0 +1,108 @@
+const cards = document.querySelectorAll(".gameContainer__container");
+const gameContainer = document.querySelector(".gameContainer");
+
+const assemblyCards = () => {
+    if (gameContainer.classList.contains("gameContainer--12")) {
+
+    } else if (gameContainer.classList.contains("gameContainer--16")) {
+
+    } else if (gameContainer.classList.contains("gameContainer--20")) {
+
+    }
+}
+
+cards[0].addEventListener("click", () => {
+    if (cards[0].classList.contains("turn")) {    
+        cards[0].classList.remove("turn")
+    } else {
+        cards[0].classList.add("turn")
+    }
+})
+
+cards[1].addEventListener("click", () => {
+    if (cards[1].classList.contains("turn")) {    
+        cards[1].classList.remove("turn")
+    } else {
+        cards[1].classList.add("turn")
+    }
+})
+
+cards[2].addEventListener("click", () => {
+    if (cards[2].classList.contains("turn")) {    
+        cards[2].classList.remove("turn")
+    } else {
+        cards[2].classList.add("turn")
+    }
+})
+
+cards[3].addEventListener("click", () => {
+    if (cards[3].classList.contains("turn")) {    
+        cards[3].classList.remove("turn")
+    } else {
+        cards[3].classList.add("turn")
+    }
+})
+
+cards[4].addEventListener("click", () => {
+    if (cards[4].classList.contains("turn")) {    
+        cards[4].classList.remove("turn")
+    } else {
+        cards[4].classList.add("turn")
+    }
+})
+
+cards[5].addEventListener("click", () => {
+    if (cards[5].classList.contains("turn")) {    
+        cards[5].classList.remove("turn")
+    } else {
+        cards[5].classList.add("turn")
+    }
+})
+
+cards[6].addEventListener("click", () => {
+    if (cards[6].classList.contains("turn")) {    
+        cards[6].classList.remove("turn")
+    } else {
+        cards[6].classList.add("turn")
+    }
+})
+
+cards[7].addEventListener("click", () => {
+    if (cards[7].classList.contains("turn")) {    
+        cards[7].classList.remove("turn")
+    } else {
+        cards[7].classList.add("turn")
+    }
+})
+
+cards[8].addEventListener("click", () => {
+    if (cards[8].classList.contains("turn")) {    
+        cards[8].classList.remove("turn")
+    } else {
+        cards[8].classList.add("turn")
+    }
+})
+
+cards[9].addEventListener("click", () => {
+    if (cards[9].classList.contains("turn")) {    
+        cards[9].classList.remove("turn")
+    } else {
+        cards[9].classList.add("turn")
+    }
+})
+
+cards[10].addEventListener("click", () => {
+    if (cards[10].classList.contains("turn")) {    
+        cards[10].classList.remove("turn")
+    } else {
+        cards[10].classList.add("turn")
+    }
+})
+
+cards[11].addEventListener("click", () => {
+    if (cards[11].classList.contains("turn")) {    
+        cards[11].classList.remove("turn")
+    } else {
+        cards[11].classList.add("turn")
+    }
+})
