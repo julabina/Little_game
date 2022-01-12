6480e4e2835e741df7e4a93469ba62fbc1608146
Lenfumé Julien <julabina@hotmail.fr>
2022-01-12 16:16:44 +0100

    app.js 80%

--- games/MEMORY/scripts/app.js
+++ games/MEMORY/scripts/app.js
@@ -1,108 +1,75 @@
-const cards = document.querySelectorAll(".gameContainer__container");
 const gameContainer = document.querySelector(".gameContainer");
 
-const assemblyCards = () => {
-    if (gameContainer.classList.contains("gameContainer--12")) {
+let cardID = [];
+let cardIDRandom = [];
+let val, nbrCard = 0;
+let countTurn = 0;
 
-    } else if (gameContainer.classList.contains("gameContainer--16")) {
+const verifyWin = () => {
+    const cards = document.querySelectorAll(".gameContainer__container");
+        console.log('---');
+        for (let i = 0;i < cards.length; i++) {
+            cards[i].classList.remove("turn");
+        }
+}
 
-    } else if (gameContainer.classList.contains("gameContainer--20")) {
+const turn = (val) => {
+    console.log(countTurn);
+    const cards = document.querySelectorAll(".gameContainer__container");
+    if (!cards[val].classList.contains("turn")) { 
+        console.log('test');
+        countTurn++;
+        if (countTurn < 3) {
+                cards[val].classList.add("turn")
+        }
+    } 
+    if (countTurn === 2) {
+        setTimeout(() => {
+            verifyWin();
+            countTurn = 0;
+        }, 2000);
+    }
+}
+    
+const randomID = (nbr) => {
+    rand = Math.ceil(Math.random() * nbr);
+    val = rand - 1;
+}
 
+const createCard = () => {
+    cardIDRandom = [];
+    for (let i = 0; i < nbrCard; i++) {
+        randomID(cardID.length);
+        cardIDRandom.push(cardID[val]);
+        cardID.splice(val, 1);
+    }
+    for (let i = 0; i < cardIDRandom.length; i++) {
+        gameContainer.innerHTML += `
+        <div class="gameContainer__container gameContainer__container--${cardIDRandom[i]}" onClick={turn(${i})}>
+        <div class="gameContainer__container__cards">
+        <div class="gameContainer__container__cards__front">
+        <img src="./assets/cardBack.webp" alt="card back">
+        </div>
+        <div class="gameContainer__container__cards__back">
+        <p>${cardIDRandom[i]}</p>
+        </div>
+        </div>
+        </div>
+        `; 
     }
 }
 
-cards[0].addEventListener("click", () => {
-    if (cards[0].classList.contains("turn")) {    
-        cards[0].classList.remove("turn")
-    } else {
-        cards[0].classList.add("turn")
+const assemblyCards = () => {
+    if (gameContainer.classList.contains("gameContainer--12")) {
+        cardID = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6];
+        nbrCard = 12;
+        createCard();
+    } else if (gameContainer.classList.contains("gameContainer--16")) {
+        cardID = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+    } else if (gameContainer.classList.contains("gameContainer--20")) {
+        cardID = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10];
     }
-})
+}
 
-cards[1].addEventListener("click", () => {
-    if (cards[1].classList.contains("turn")) {    
-        cards[1].classList.remove("turn")
-    } else {
-        cards[1].classList.add("turn")
-    }
-})
+assemblyCards();
 
-cards[2].addEventListener("click", () => {
-    if (cards[2].classList.contains("turn")) {    
-        cards[2].classList.remove("turn")
-    } else {
-        cards[2].classList.add("turn")
-    }
-})
-
-cards[3].addEventListener("click", () => {
-    if (cards[3].classList.contains("turn")) {    
-        cards[3].classList.remove("turn")
-    } else {
-        cards[3].classList.add("turn")
-    }
-})
-
-cards[4].addEventListener("click", () => {
-    if (cards[4].classList.contains("turn")) {    
-        cards[4].classList.remove("turn")
-    } else {
-        cards[4].classList.add("turn")
-    }
-})
-
-cards[5].addEventListener("click", () => {
-    if (cards[5].classList.contains("turn")) {    
-        cards[5].classList.remove("turn")
-    } else {
-        cards[5].classList.add("turn")
-    }
-})
-
-cards[6].addEventListener("click", () => {
-    if (cards[6].classList.contains("turn")) {    
-        cards[6].classList.remove("turn")
-    } else {
-        cards[6].classList.add("turn")
-    }
-})
-
-cards[7].addEventListener("click", () => {
-    if (cards[7].classList.contains("turn")) {    
-        cards[7].classList.remove("turn")
-    } else {
-        cards[7].classList.add("turn")
-    }
-})
-
-cards[8].addEventListener("click", () => {
-    if (cards[8].classList.contains("turn")) {    
-        cards[8].classList.remove("turn")
-    } else {
-        cards[8].classList.add("turn")
-    }
-})
-
-cards[9].addEventListener("click", () => {
-    if (cards[9].classList.contains("turn")) {    
-        cards[9].classList.remove("turn")
-    } else {
-        cards[9].classList.add("turn")
-    }
-})
-
-cards[10].addEventListener("click", () => {
-    if (cards[10].classList.contains("turn")) {    
-        cards[10].classList.remove("turn")
-    } else {
-        cards[10].classList.add("turn")
-    }
-})
-
-cards[11].addEventListener("click", () => {
-    if (cards[11].classList.contains("turn")) {    
-        cards[11].classList.remove("turn")
-    } else {
-        cards[11].classList.add("turn")
-    }
-})
