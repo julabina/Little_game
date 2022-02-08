e2cda4fa9b5563ab966ebb49a3be9e12fc485c3c
Lenfumé Julien <julabina@hotmail.fr>
2022-02-08 10:55:37 +0100

    js logic + modale

--- games/THE_PRICE_IS_RIGHT/script/app.js
+++ games/THE_PRICE_IS_RIGHT/script/app.js
@@ -15,7 +15,9 @@ const modaleInput = document.querySelector(".modaleP2__cont__input");
 const modaleBtn = document.querySelector(".modaleP2__cont__btn");
 const modaleMsg = document.querySelector(".modaleP2__cont__msg");
 
-let goodPrice, playerPrice, turn, time, val;
+let goodPrice, playerPrice, turn, time, val, timerInterval ;
+let gameOn = false;
+let turnMod = true;
 
 const randomPrice = (diff) => {
    let val = Math.ceil(Math.random() * diff);
@@ -25,49 +27,57 @@ const randomPrice = (diff) => {
 const gameOption = () => {
     // game mod
     if (radiosTurn[0].checked) {
+        turnMod = true;
         turn = 20;
         turnLeft.textContent = "Turn left : " + turn;
         timer.textContent = "";
     } else if (radiosTurn[1].checked) {
+        turnMod = true;
         turn = 10;
         turnLeft.textContent = "Turn left : " + turn;
         timer.textContent = "";
     } else if (radiosTurn[2].checked) {
+        turnMod = true;
         turn = 5;
         turnLeft.textContent = "Turn left : " + turn;
         timer.textContent = "";
     } else if (radiosTurn[3].checked) {
+        turnMod = true;
         turn = 3;
         turnLeft.textContent = "Turn left : " + turn;
         timer.textContent = "";
     } else if (radiosTimer[0].checked) {
+        turnMod = false;
         time = 120;
         turnLeft.textContent = "";
         timer.textContent = "Time : " + time;
     } else if (radiosTimer[1].checked) {
+        turnMod = false;
         time = 60;
         turnLeft.textContent = "";
         timer.textContent = "Time : " + time;
     } else if (radiosTimer[2].checked) {
+        turnMod = false;
         time = 40;
         turnLeft.textContent = "";
         timer.textContent = "Time : " + time;
     } else if (radiosTimer[3].checked) {
+        turnMod = false;
         time = 20;
         turnLeft.textContent = "";
         timer.textContent = "Time : " + time;
     } 
     // other option
-        if (radiosDifficulty[0].checked) {
-            val = 100;
-        } else if (radiosDifficulty[1].checked) {
-            val = 1000;
-        } else if (radiosDifficulty[2].checked) {
+    if (radiosDifficulty[0].checked) {
+        val = 100;
+    } else if (radiosDifficulty[1].checked) {
+        val = 1000;
+    } else if (radiosDifficulty[2].checked) {
             val = 10000;
         } else if (radiosDifficulty[3].checked) {
             val = 100000;
         }
-    if (radiosPlayers[1].checked) {
+        if (radiosPlayers[1].checked) {
         modale.classList.remove("modaleP2--off");
         modaleInput.focus();
         modaleMsg.textContent = "Enter a number between 0 and " + val;
@@ -76,11 +86,47 @@ const gameOption = () => {
         inputPropose.classList.remove("game__top__input--off");
         proposeBtn.classList.remove("game__mid__btn--off");
         inputPropose.focus();
+        if (turnMod === false) {
+            timerGo();
+        }
+    }
+}
+
+const timerGo = () => {
+        timerInterval = setInterval(() => {
+        time--;
+        timer.textContent = "Time : " + time;
+        if (time < 1) {
+            console.log('fini');
+            clearInterval(timerInterval);
+        }
+    }, 1000);
+}
+
+const verifyLose = () => {
+    if (turnMod === true) {
+        turn--;
+        turnLeft.textContent = "Turn left : " + turn;
+        if (turn < 1) {
+            resultContainer.innerHTML += `
+            <div class="game__bot__status game__bot__status--lose">LOSE</div>
+            <div class="game__bot__good">The good price was ${goodPrice}</div>
+            `;
+            resultContainer.scrollTop = resultContainer.scrollHeight;
+            less.innerHTML = ``;
+            more.innerHTML = ``;
+            inputPropose.classList.add("game__top__input--off");
+            proposeBtn.classList.add("game__mid__btn--off");
+            gameOn = false;
+        }
     }
 }
 
 const verifyPrice = (playerPrice) => {
     if (playerPrice === goodPrice) {
+        if (turnMod === false) {
+            clearInterval(timerInterval);
+        }
         resultContainer.innerHTML += `
         <div class="game__bot__good">${playerPrice}</div>
         <div class="game__bot__status game__bot__status--win">WIN</div>
@@ -90,7 +136,7 @@ const verifyPrice = (playerPrice) => {
         more.innerHTML = ``;
         inputPropose.classList.add("game__top__input--off");
         proposeBtn.classList.add("game__mid__btn--off");
-        playBtn.focus();
+        gameOn = false;
     } else if (playerPrice < goodPrice) {
         resultContainer.innerHTML += `
         <div class="game__bot__more">${playerPrice}</div>
@@ -98,6 +144,7 @@ const verifyPrice = (playerPrice) => {
         resultContainer.scrollTop = resultContainer.scrollHeight;
         less.innerHTML = ``;
         more.innerHTML = `<p>MORE</p>`;
+        verifyLose();
     } else {
         resultContainer.innerHTML += `
         <div class="game__bot__less">${playerPrice}</div>
@@ -105,23 +152,36 @@ const verifyPrice = (playerPrice) => {
         resultContainer.scrollTop = resultContainer.scrollHeight;
         less.innerHTML = `<p>LESS</p>`;
         more.innerHTML = ``;
+        verifyLose();
     }
 }
 
 const p2Chose = () => {
     if (modaleInput.value !== "") {
-        if (parseInt(modaleInput.value) >! val) {
+        if (parseInt(modaleInput.value) <= val) {
+            modaleInput.classList.remove("modaleP2__cont__input--error");
             goodPrice = parseInt(modaleInput.value)
             inputPropose.classList.remove("game__top__input--off");
             proposeBtn.classList.remove("game__mid__btn--off");
             inputPropose.focus();
             modale.classList.add("modaleP2--off");
+            if (turnMod === false) {
+                setTimeout(() => {
+                    timerGo();
+                }, 1200);
+            }
+        } else {
+            modaleInput.classList.add("modaleP2__cont__input--error");
+            modaleInput.focus();
         }
+    } else {
+        modaleInput.classList.add("modaleP2__cont__input--error");
+        modaleInput.focus();
     }
 }
 
 proposeBtn.addEventListener("click", () => {
-    if (inputPropose.value !== "") {
+    if (inputPropose.value !== "" && gameOn === true) {
         verifyPrice(parseInt(inputPropose.value));
         inputPropose.value = '';
     }
@@ -130,7 +190,7 @@ proposeBtn.addEventListener("click", () => {
 
 inputPropose.addEventListener("keypress", (e) => {
     if (e.code === 'Enter') {
-        if (inputPropose.value !== "") {
+        if (inputPropose.value !== "" && gameOn === true) {
             verifyPrice(parseInt(inputPropose.value));
             inputPropose.value = '';
         }
@@ -139,10 +199,19 @@ inputPropose.addEventListener("keypress", (e) => {
 })
 
 playBtn.addEventListener("click", () => {
+    clearInterval(timerInterval);
     resultContainer.innerHTML = ``;
+    inputPropose.value = '';
     gameOption();
+    gameOn = true;
 })
 
 modaleBtn.addEventListener("click", () => {
     p2Chose();
+})
+
+modaleInput.addEventListener("keypress", (e) => {
+    if (e.code === 'Enter') {
+        p2Chose();
+    }
 })
